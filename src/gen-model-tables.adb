9cab4992c0ee03de20baaeadb820f8b303dc022e
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-09-08 19:46:05 +0000

    Update the model to follow Hibernate XML description as much as possible - use hibernate XML nodes and attributes - prepare to generate inheritance support - add more attributes on the column nodes - remove old unused method

--- src/gen-model-tables.adb
+++ src/gen-model-tables.adb
@@ -16,8 +16,13 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
+with Ada.Strings;
+with Ada.Strings.Maps;
 with DOM.Core.Nodes;
 with DOM.Core.Elements;
+with Gen.Utils;
+with Util.Strings;
+with Util.Strings.Transforms;
 package body Gen.Model.Tables is
 
    use type DOM.Core.Node;
@@ -36,43 +41,33 @@ package body Gen.Model.Tables is
       elsif Name = "index" then
          return EL.Objects.To_Object (From.Number);
 
+      elsif Name = "isUnique" then
+         return EL.Objects.To_Object (From.Unique);
+
+      elsif Name = "isNull" then
+         return EL.Objects.To_Object (not From.Not_Null);
+
+      elsif Name = "sqlType" then
+         return EL.Objects.To_Object (From.Sql_Type);
+
+      elsif Name = "sqlName" then
+         return EL.Objects.To_Object (From.Sql_Name);
+
+      elsif Name = "isVersion" then
+         return EL.Objects.To_Object (From.Is_Version);
+
+      elsif Name = "isPrimaryKey" then
+         return EL.Objects.To_Object (From.Is_Key);
+
       else
          return Definition (From).Get_Value (Name);
       end if;
    end Get_Value;
 
-   --  ------------------------------
-   --  Set the DOM node associated with the definition object
-   --  ------------------------------
-   overriding
-   procedure Set_Node (O     : in out Column_Definition;
-                       Node  : in DOM.Core.Node;
-                       Index : in Natural) is
-   begin
-      O.Node := Node;
-      O.Number := Index;
-   end Set_Node;
-
    procedure Initialize (O : in out Table_Definition) is
    begin
       O.Members_Bean := EL.Objects.To_Object (O.Members'Unchecked_Access);
    end Initialize;
-
-   --  ------------------------------
-   --  Set the DOM node associated with the definition object
-   --  ------------------------------
-   overriding
-   procedure Set_Node (O     : in out Table_Definition;
-                       Node  : in DOM.Core.Node;
-                       Index : in Natural) is
-      pragma Unreferenced (Index);
-
-      Nodes : constant DOM.Core.Node_List
-        := DOM.Core.Elements.Get_Elements_By_Tag_Name (Node, "column");
-   begin
-      O.Node := Node;
-      O.Members.Set_List (Nodes);
-   end Set_Node;
 
 --     function Get_Member_Sql_Mode (Member : in DOM.Core.Node) return String is
 --        Type_Name : constant DOM_String := Get_Attribute (Member, "type");
@@ -102,94 +97,6 @@ package body Gen.Model.Tables is
 --        end if;
 --     end Get_Member_Value;
 
-   --  ------------------------------
-   --  Get the primary key for this table
-   --  ------------------------------
-   function Get_Primary_Key (From : Table_Definition) return DOM.Core.Node is
-      Nodes : constant DOM.Core.Node_List := From.Members.Get_List;
-      Count : constant Natural := DOM.Core.Nodes.Length (Nodes);
-   begin
-      for I in 0 .. Count - 1 loop
-         declare
-            N : constant DOM.Core.Node := DOM.Core.Nodes.Item (Nodes, I);
-            V : constant DOM.Core.DOM_String := DOM.Core.Elements.Get_Attribute (N, "primary-key");
-         begin
-            if V = "true" then
-               return N;
-            end if;
-         end;
-      end loop;
-      return null;
-   end Get_Primary_Key;
-
-   --  ------------------------------
-   --  Get the primary key type for this table
-   --  ------------------------------
-   function Get_Primary_Key_Type (From : Table_Definition) return String is
-      Node : constant DOM.Core.Node := From.Get_Primary_Key;
-   begin
-      if Node /= null then
-         return DOM.Core.Elements.Get_Attribute (Node, "type");
-      else
-         return "Identifier";
-      end if;
-   end Get_Primary_Key_Type;
-
-   --  ------------------------------
-   --  Get the primary key type for this table
-   --  ------------------------------
-   function Get_Primary_Key_Name (From : Table_Definition) return String is
-      Node : constant DOM.Core.Node := From.Get_Primary_Key;
-   begin
-      if Node /= null then
-         return DOM.Core.Elements.Get_Attribute (Node, "name");
-      else
-         return "id";
-      end if;
-   end Get_Primary_Key_Name;
-
-   VERSION_NAME : constant String := "version";
-
-   --  ------------------------------
-   --  Get the DOM node <b>version</b> if there is one.
-   --  ------------------------------
-   function Get_Version_Column (From : Table_Definition) return DOM.Core.Node is
-      --  Check if there is a version column to generate.
-      Nodes : constant DOM.Core.Node_List
-        := DOM.Core.Elements.Get_Elements_By_Tag_Name (From.Node, VERSION_NAME);
-   begin
-      if DOM.Core.Nodes.Length (Nodes) > 0 then
-         return DOM.Core.Nodes.Item (Nodes, 0);
-      else
-         return null;
-      end if;
-   end Get_Version_Column;
-
-   --  ------------------------------
-   --  Get the <b>version</b> column name used by the lazy lock implementation
-   --  ------------------------------
-   function Get_Version_Column_Name (From : Table_Definition) return String is
-      Node : constant DOM.Core.Node := From.Get_Version_Column;
-   begin
-      if Node /= null then
-         return DOM.Core.Elements.Get_Attribute (Node, "name");
-      else
-         return "";
-      end if;
-   end Get_Version_Column_Name;
-
-   --  ------------------------------
-   --  Get the <b>version</b> column type used by the lazy lock implementation
-   --  ------------------------------
-   function Get_Version_Column_Type (From : Table_Definition) return String is
-      Node : constant DOM.Core.Node := From.Get_Version_Column;
-   begin
-      if Node /= null then
-         return DOM.Core.Elements.Get_Attribute (Node, "type");
-      else
-         return "";
-      end if;
-   end Get_Version_Column_Type;
 
    --  ------------------------------
    --  Get the value identified by the name.
@@ -201,30 +108,49 @@ package body Gen.Model.Tables is
       if Name = "members" then
          return From.Members_Bean;
 
-      elsif Name = "primaryKeyType" then
-         return EL.Objects.To_Object (From.Get_Primary_Key_Type);
+      elsif Name ="id" then
+         declare
+            Bean : constant EL.Beans.Readonly_Bean_Access := From.Id_Column.all'Access;
+         begin
+            return EL.Objects.To_Object (Bean);
+         end;
 
-      elsif Name = "primaryKeyName" then
-         return EL.Objects.To_Object (From.Get_Primary_Key_Name);
+      elsif Name ="version" then
+         declare
+            Bean : constant EL.Beans.Readonly_Bean_Access := From.Version_Column.all'Access;
+         begin
+            return EL.Objects.To_Object (Bean);
+         end;
 
-      elsif Name = "version" then
-         return EL.Objects.To_Object (From.Get_Version_Column_Name);
-
-      elsif Name = "versionType" then
-         return EL.Objects.To_Object (From.Get_Version_Column_Type);
-
-      elsif Name ="versionIndex" then
-         return EL.Objects.To_Object (DOM.Core.Nodes.Length (From.Members.Get_List) + 1);
+      elsif Name = "type" then
+         return EL.Objects.To_Object (From.Type_Name);
 
       else
          return Definition (From).Get_Value (Name);
       end if;
    end Get_Value;
 
-   procedure Initialize (O : in out Model_Definition) is
+   --  ------------------------------
+   --  Get the value identified by the name.
+   --  If the name cannot be found, the method should return the Null object.
+   --  ------------------------------
+   overriding
+   function Get_Value (From : Package_Definition;
+                       Name : String) return EL.Objects.Object is
    begin
-      O.Tables_Bean := EL.Objects.To_Object (O.Tables'Unchecked_Access);
-   end Initialize;
+     if Name = "name" then
+        return EL.Objects.To_Object (From.Pkg_Name);
+
+     elsif Name = "package" then
+        return EL.Objects.To_Object (From.Base_Name);
+
+     elsif Name = "tables" then
+        return From.Tables_Bean;
+
+     else
+        return Definition (From).Get_Value (Name);
+     end if;
+   end Get_Value;
 
    --  ------------------------------
    --  Get the value identified by the name.
@@ -240,13 +166,131 @@ package body Gen.Model.Tables is
       end if;
    end Get_Value;
 
+   --  ------------------------------
+   --  Register the column definition in the table
+   --  ------------------------------
+   procedure Register_Column (Table  : in out Table_Definition;
+                              Column : in DOM.Core.Node) is
+      Name : constant DOM.Core.DOM_String      := DOM.Core.Nodes.Node_Name (Column);
+      C    : constant Column_Definition_Access := new Column_Definition;
+   begin
+      C.Node := Column;
+      C.Number := Table.Members.Get_Count;
+      Table.Members.Append (C);
+
+      --  Get the SQL mapping from an optional <column> element.
+      declare
+         N : DOM.Core.Node := Get_Child (Column, "column");
+      begin
+         if N /= null then
+            C.Sql_Name := Get_Attribute (N, "name");
+            C.Sql_Type := Get_Attribute (N, "sql-type");
+         else
+            N := Column;
+            C.Sql_Name := Get_Attribute (N, "column");
+            C.Sql_Type := Get_Attribute (N, "type");
+         end if;
+         C.Not_Null := Get_Attribute (N, "not-null");
+         C.Unique   := Get_Attribute (N, "unique");
+      end;
+      if Name = "version" then
+         Table.Version_Column := C;
+         C.Is_Version := True;
+
+      elsif Name = "id" then
+         Table.Id_Column := C;
+         C.Is_Key := True;
+
+      end if;
+   end Register_Column;
+
+   --  ------------------------------
+   --  Register all the columns defined in the table
+   --  ------------------------------
+   procedure Register_Columns (Table : in out Table_Definition) is
+      procedure Iterate is new Gen.Utils.Iterate_Nodes (T       => Table_Definition,
+                                                        Process => Register_Column);
+   begin
+      Iterate (Table, Table.Node, "id");
+      Iterate (Table, Table.Node, "version");
+      Iterate (Table, Table.Node, "property");
+   end Register_Columns;
+
+   --  ------------------------------
+   --  Register or find the package knowing its name
+   --  ------------------------------
+   procedure Register_Package (O      : in out Model_Definition;
+                               Name   : in Unbounded_String;
+                               Result : out Package_Definition_Access) is
+      Pos : constant Package_Map.Cursor := O.Packages.Find (Name);
+   begin
+      if not Package_Map.Has_Element (Pos) then
+         declare
+            Map : Ada.Strings.Maps.Character_Mapping;
+            Base_Name : Unbounded_String;
+         begin
+            Map := Ada.Strings.Maps.To_Mapping (From => ".", To => "-");
+            Base_Name := Translate (Name, Map);
+
+            Result := new Package_Definition;
+            Result.Pkg_Name := Name;
+            Result.Tables_Bean := EL.Objects.To_Object (Result.Tables'Access);
+            Util.Strings.Transforms.To_Lower_Case (To_String (Base_Name),
+                                                   Result.Base_Name);
+            O.Packages.Insert (Name, Result);
+         end;
+      else
+         Result := Package_Map.Element (Pos);
+      end if;
+   end Register_Package;
+
+   --  ------------------------------
+   --  Register a new class definition in the model.
+   --  ------------------------------
+   procedure Register_Class (O    : in out Model_Definition;
+                             Node : in DOM.Core.Node) is
+      Table : constant Table_Definition_Access := new Table_Definition;
+   begin
+      Table.Node := Node;
+      Table.Name := Table.Get_Attribute ("name");
+      declare
+         Pos : constant Natural := Index (Table.Name, ".", Ada.Strings.Backward);
+      begin
+         if Pos > 0 then
+            Table.Pkg_Name := Unbounded_Slice (Table.Name, 1, Pos - 1);
+            Table.Type_Name := Unbounded_Slice (Table.Name, Pos + 1, Length (Table.Name));
+         else
+            Table.Pkg_Name := To_Unbounded_String ("ADO");
+            Table.Type_Name := Table.Name;
+         end if;
+      end;
+
+      O.Register_Package (Table.Pkg_Name, Table.Package_Def);
+      Table.Package_Def.Tables.Append (Table);
+      Table.Register_Columns;
+
+      O.Tables.Append (Table);
+   end Register_Class;
+
    procedure Initialize (O : in out Model_Definition;
                          N : in DOM.Core.Node) is
-      Nodes : constant DOM.Core.Node_List
-        := DOM.Core.Elements.Get_Elements_By_Tag_Name (N, "table");
+
+      procedure Iterate is new Gen.Utils.Iterate_Nodes (T => Model_Definition,
+                                                        Process => Register_Class);
+
+      T : constant EL.Beans.Readonly_Bean_Access := O.Tables'Unchecked_Access;
    begin
-      O.Set_Node (N);
-      O.Tables.Set_List (Nodes);
+      O.Tables_Bean := EL.Objects.To_Object (T);
+      Iterate (O, N, "class");
+      Iterate (O, N, "subclass");
    end Initialize;
+
+   --  ------------------------------
+   --  Get the first package of the model definition.
+   --  ------------------------------
+   function First (From : Model_Definition) return Package_Cursor is
+   begin
+      return From.Packages.First;
+   end First;
 
 end Gen.Model.Tables;
--- src/gen-model-tables.ads
+++ src/gen-model-tables.ads
@@ -17,13 +17,19 @@
 -----------------------------------------------------------------------
 with EL.Objects;
 
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded.Hash;
 with Gen.Model.List;
 package Gen.Model.Tables is
+
+   use Ada.Strings.Unbounded;
 
    --  ------------------------------
    --  Column Definition
    --  ------------------------------
    type Column_Definition is new Definition with private;
+   type Column_Definition_Access is access all Column_Definition'Class;
 
    --  Get the value identified by the name.
    --  If the name cannot be found, the method should return the Null object.
@@ -31,16 +37,11 @@ package Gen.Model.Tables is
    function Get_Value (From : Column_Definition;
                        Name : String) return EL.Objects.Object;
 
-   --  Set the DOM node associated with the definition object
-   overriding
-   procedure Set_Node (O     : in out Column_Definition;
-                       Node  : in DOM.Core.Node;
-                       Index : in Natural);
-
    --  ------------------------------
    --  Table Definition
    --  ------------------------------
    type Table_Definition is new Definition with private;
+   type Table_Definition_Access is access all Table_Definition'Class;
 
    --  Get the value identified by the name.
    --  If the name cannot be found, the method should return the Null object.
@@ -48,34 +49,28 @@ package Gen.Model.Tables is
    function Get_Value (From : Table_Definition;
                        Name : String) return EL.Objects.Object;
 
-   --  Set the DOM node associated with the definition object
+   --  Register all the columns defined in the table
+   procedure Register_Columns (Table : in out Table_Definition);
+
+   --  ------------------------------
+   --  Package Definition
+   --  ------------------------------
+   type Package_Definition is new Definition with private;
+   type Package_Definition_Access is access all Package_Definition'Class;
+
+   --  Get the value identified by the name.
+   --  If the name cannot be found, the method should return the Null object.
    overriding
-   procedure Set_Node (O     : in out Table_Definition;
-                       Node  : in DOM.Core.Node;
-                       Index : in Natural);
-
-   --  Get the primary key for this table
-   function Get_Primary_Key (From : Table_Definition) return DOM.Core.Node;
-
-   --  Get the primary key type for this table
-   function Get_Primary_Key_Type (From : Table_Definition) return String;
-
-   --  Get the primary key type for this table
-   function Get_Primary_Key_Name (From : Table_Definition) return String;
-
-   --  Get the DOM node <b>version</b> if there is one.
-   function Get_Version_Column (From : Table_Definition) return DOM.Core.Node;
-
-   --  Get the <b>version</b> column name used by the lazy lock implementation
-   function Get_Version_Column_Name (From : Table_Definition) return String;
-
-   --  Get the <b>version</b> column type used by the lazy lock implementation
-   function Get_Version_Column_Type (From : Table_Definition) return String;
+   function Get_Value (From : Package_Definition;
+                       Name : String) return EL.Objects.Object;
 
    --  ------------------------------
    --  Model Definition
    --  ------------------------------
+   --  The <b>Model_Definition</b> contains the complete model from one or
+   --  several files.
    type Model_Definition is new Definition with private;
+   type Model_Definition_Access is access all Model_Definition'Class;
 
    --  Get the value identified by the name.
    --  If the name cannot be found, the method should return the Null object.
@@ -86,28 +81,122 @@ package Gen.Model.Tables is
    procedure Initialize (O : in out Model_Definition;
                          N : in DOM.Core.Node);
 
+   --  Register or find the package knowing its name
+   procedure Register_Package (O      : in out Model_Definition;
+                               Name   : in Unbounded_String;
+                               Result : out Package_Definition_Access);
+
+   --  Register a new class definition in the model.
+   procedure Register_Class (O    : in out Model_Definition;
+                             Node : in DOM.Core.Node);
+
+   package Package_Map is
+     new Ada.Containers.Indefinite_Hashed_Maps (Key_Type        => Unbounded_String,
+                                                Element_Type    => Package_Definition_Access,
+                                                Hash            => Ada.Strings.Unbounded.Hash,
+                                                Equivalent_Keys => "=");
+
+   subtype Package_Cursor is Package_Map.Cursor;
+
+   --  Get the first package of the model definition.
+   function First (From : Model_Definition) return Package_Cursor;
+
+   --  Returns true if the package cursor contains a valid package
+   function Has_Element (Position : Package_Cursor) return Boolean
+     renames Package_Map.Has_Element;
+
+   --  Returns the package definition.
+   function Element (Position : Package_Cursor) return Package_Definition_Access
+     renames Package_Map.Element;
+
+   --  Move the iterator to the next package definition.
+   procedure Next (Position : in out Package_Cursor)
+     renames Package_Map.Next;
+
+--     function Find_Class (From : Model_Definition;
+--                          Name : String) return Table_Definition;
+
+
+   package Table_Map is
+     new Ada.Containers.Indefinite_Hashed_Maps (Key_Type        => Unbounded_String,
+                                                Element_Type    => Table_Definition_Access,
+                                                Hash            => Ada.Strings.Unbounded.Hash,
+                                                Equivalent_Keys => "=");
+
+   subtype Table_Cursor is Table_Map.Cursor;
+
+   --  Returns true if the table cursor contains a valid table
+   function Has_Element (Position : Table_Cursor) return Boolean
+     renames Table_Map.Has_Element;
+
+   --  Returns the table definition.
+   function Element (Position : Table_Cursor) return Table_Definition_Access
+     renames Table_Map.Element;
+
+   --  Move the iterator to the next table definition.
+   procedure Next (Position : in out Table_Cursor)
+     renames Table_Map.Next;
+
+--     function Find_Class (From : Model_Definition;
+--                          Name : String) return Table_Definition;
+
 private
 
    type Column_Definition is new Definition with record
       Number : Natural := 0;
+      Table  : Table_Definition_Access;
+
+      --  Whether the column must not be null in the database
+      Not_Null : Boolean := False;
+
+      --  Whether the column must be unique
+      Unique   : Boolean := False;
+
+      --  The SQL type associated with the column.
+      Sql_Type : Unbounded_String;
+
+      --  The SQL name associated with the column.
+      Sql_Name : Unbounded_String;
+
+      --  True if this column is the optimistic locking version column.
+      Is_Version : Boolean := False;
+
+      --  True if this column is the primary key column.
+      Is_Key : Boolean := False;
    end record;
 
-   package Column_List is new Gen.Model.List (Column_Definition);
+   package Column_List is new Gen.Model.List (T         => Column_Definition,
+                                              T_Access  => Column_Definition_Access);
 
    procedure Initialize (O : in out Table_Definition);
 
    type Table_Definition is new Definition with record
-      Members      : aliased Column_List.List_Definition;
-      Members_Bean : EL.Objects.Object;
+      Members        : aliased Column_List.List_Definition;
+      Members_Bean   : EL.Objects.Object;
+      Parent         : Table_Definition_Access;
+      Package_Def    : Package_Definition_Access;
+      Name           : Unbounded_String;
+      Type_Name      : Unbounded_String;
+      Pkg_Name       : Unbounded_String;
+      Version_Column : Column_Definition_Access;
+      Id_Column      : Column_Definition_Access;
    end record;
 
-   package Table_List is new Gen.Model.List (Table_Definition);
+   package Table_List is new Gen.Model.List (T        => Table_Definition,
+                                             T_Access => Table_Definition_Access);
 
-   procedure Initialize (O : in out Model_Definition);
+   type Package_Definition is new Definition with record
+      Tables      : aliased Table_List.List_Definition;
+      Tables_Bean : EL.Objects.Object;
+      Pkg_Name    : Unbounded_String;
+      Name        : Unbounded_String;
+      Base_Name   : Unbounded_String;
+   end record;
 
    type Model_Definition is new Definition with record
       Tables      : aliased Table_List.List_Definition;
       Tables_Bean : EL.Objects.Object;
+      Packages    : Package_Map.Map;
    end record;
 
 end Gen.Model.Tables;

bf57a7c94840452b4f1fd40ea357c2dda55262ef
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-02 16:36:02 +0000

    Add the <<Nullable>> and <<Not Null>> stereotypes

--- src/gen-artifacts-xmi.ads
+++ src/gen-artifacts-xmi.ads
@@ -67,6 +67,8 @@ private
       PK_Stereotype         : Gen.Model.XMI.Stereotype_Element_Access;
       FK_Stereotype         : Gen.Model.XMI.Stereotype_Element_Access;
       Data_Model_Stereotype : Gen.Model.XMI.Stereotype_Element_Access;
+      Nullable_Stereotype   : Gen.Model.XMI.Stereotype_Element_Access;
+      Not_Null_Stereotype   : Gen.Model.XMI.Stereotype_Element_Access;
 
       --  Tag definitions which control the generation.
       Has_List_Tag          : Gen.Model.XMI.Tag_Definition_Element_Access;

cfc483bb49e19cbbba1fbde04f1a5e415ca576fa
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-06 19:57:44 +0000

    Recognize the isBean attribute on a table

--- src/gen-model-tables.adb
+++ src/gen-model-tables.adb
@@ -298,7 +298,7 @@ package body Gen.Model.Tables is
       elsif Name = "table" then
          return Util.Beans.Objects.To_Object (From.Table_Name);
 
-      elsif Name = "isVersion" or Name = "isPrimaryKey"
+      elsif Name = "isVersion" or Name = "isPrimaryKey" or Name = "isBean"
         or Name = "isPrimitiveType" or Name = "isEnum" or Name = "isIdentifier"
         or Name = "isBoolean" or Name = "isBlob" or Name = "isDate" or Name = "isString" then
          return Util.Beans.Objects.To_Object (False);

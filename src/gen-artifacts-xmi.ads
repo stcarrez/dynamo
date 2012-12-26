a3302546c1698b49330fbe0942bb36334a8a2b80
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-26 17:56:45 +0000

    Get the ADO.@dynamo.sql.length tag definition

--- src/gen-artifacts-xmi.ads
+++ src/gen-artifacts-xmi.ads
@@ -83,6 +83,7 @@ private
       Sql_Type_Tag          : Gen.Model.XMI.Tag_Definition_Element_Access;
       Generator_Tag         : Gen.Model.XMI.Tag_Definition_Element_Access;
       Literal_Tag           : Gen.Model.XMI.Tag_Definition_Element_Access;
+      Sql_Length_Tag        : Gen.Model.XMI.Tag_Definition_Element_Access;
 
       --  Stereotype which triggers the generation of AWA bean types.
       Bean_Stereotype  : Gen.Model.XMI.Stereotype_Element_Access;

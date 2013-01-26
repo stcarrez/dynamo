174bafb5c13248c2196b3724028e73e9cb556812
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-26 13:34:13 +0000

    Add a copy-first command to copy the first file found only (the default is to copy the last one since this is the reverse search path)

--- src/gen-artifacts-distribs-copies.adb
+++ src/gen-artifacts-distribs-copies.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  gen-artifacts-distribs-copies -- Copy based distribution artifact
---  Copyright (C) 2012 Stephane Carrez
+--  Copyright (C) 2012, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,11 +30,13 @@ package body Gen.Artifacts.Distribs.Copies is
    --  ------------------------------
    --  Create a distribution rule to copy a set of files or directories.
    --  ------------------------------
-   function Create_Rule (Node : in DOM.Core.Node) return Distrib_Rule_Access is
+   function Create_Rule (Node : in DOM.Core.Node;
+                         Copy_First_File : in Boolean) return Distrib_Rule_Access is
       pragma Unreferenced (Node);
 
       Result : constant Copy_Rule_Access := new Copy_Rule;
    begin
+      Result.Copy_First_File := Copy_First_File;
       return Result.all'Access;
    end Create_Rule;
 
@@ -53,12 +55,18 @@ package body Gen.Artifacts.Distribs.Copies is
                       Path    : in String;
                       Files   : in File_Vector;
                       Context : in out Generator'Class) is
-      pragma Unreferenced (Rule, Context);
+      pragma Unreferenced (Context);
+      use type Ada.Containers.Count_Type;
 
-      Source : constant String := Get_First_Path (Files);
+      Source : constant String := Get_Source_Path (Files, Rule.Copy_First_File);
       Dir    : constant String := Ada.Directories.Containing_Directory (Path);
    begin
-      Log.Info ("copy {0} to {1}", Source, Path);
+      if Files.Length > 1 then
+         Log.Info ("copy {0} to {1} (ignoring {2} files)", Source, Path,
+                   Natural'Image (Natural (Files.Length) - 1));
+      else
+         Log.Info ("copy {0} to {1}", Source, Path);
+      end if;
 
       Ada.Directories.Create_Path (Dir);
       Ada.Directories.Copy_File (Source_Name => Source,

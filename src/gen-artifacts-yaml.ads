9589548827a5e6d3881ddca3f76b88a4e1592ed3
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-05-13 20:23:49 +0200

    New package for the support of YAML model files

---
+++ src/gen-artifacts-yaml.ads
@@ -0,0 +1,46 @@
+-----------------------------------------------------------------------
+--  gen-artifacts-yaml -- Yaml database model files
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Gen.Model.Packages;
+
+package Gen.Artifacts.Yaml is
+
+   --  ------------------------------
+   --  Yaml artifact
+   --  ------------------------------
+   type Artifact is new Gen.Artifacts.Artifact with private;
+
+   --  Prepare the model after all the configuration files have been read and before
+   --  actually invoking the generation.
+   overriding
+   procedure Prepare (Handler : in out Artifact;
+                      Model   : in out Gen.Model.Packages.Model_Definition'Class;
+                      Context : in out Generator'Class);
+
+   --  Read the UML/XMI model file.
+   procedure Read_Model (Handler       : in out Artifact;
+                         File          : in String;
+                         Model         : in out Gen.Model.Packages.Model_Definition;
+                         Context       : in out Generator'Class;
+                         Is_Predefined : in Boolean := False);
+
+private
+
+   type Artifact is new Gen.Artifacts.Artifact with null record;
+
+end Gen.Artifacts.Yaml;

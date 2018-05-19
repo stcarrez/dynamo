7ca7a178ef0aeebb73d4c5b30f79a477a1112fa9
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-05-19 17:25:15 +0200

    Fix compilation warnings

--- src/gen-commands-layout.adb
+++ src/gen-commands-layout.adb
@@ -30,7 +30,6 @@ package body Gen.Commands.Layout is
                       Name      : in String;
                       Args      : in Argument_List'Class;
                       Generator : in out Gen.Generator.Handler) is
-      pragma Unreferenced (Name);
       use Ada.Strings.Unbounded;
 
       function Get_Name return String;

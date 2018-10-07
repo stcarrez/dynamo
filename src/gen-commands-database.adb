49db376e09833c9df0e6f8d4c4adbdeea0f5a504
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-10-07 21:19:00 +0200

    Update the create-database command to use the ADO.Schemas.Databases.Create_Database operation

--- src/gen-commands-database.adb
+++ src/gen-commands-database.adb
@@ -25,209 +25,28 @@ with Ada.Exceptions;
 with Util.Strings;
 with Util.Files;
 with Util.Log.Loggers;
-with Util.Processes;
-with Util.Streams.Texts;
-with Util.Streams.Pipes;
+with Util.Strings.Vectors;
 
-with ADO.Drivers.Connections;
-with ADO.Sessions.Factory;
+with ADO.Drivers;
 with ADO.Sessions.Sources;
-with ADO.Statements;
-with ADO.Queries;
-with ADO.Parameters;
+with ADO.Schemas.Databases;
 
-with Gen.Database.Model;
 package body Gen.Commands.Database is
 
-   use Util.Log;
+   Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Gen.Commands.Database");
 
-   Log : constant Loggers.Logger := Loggers.Create ("Gen.Commands.Database");
+   function Get_Schema_Path (Model_Dir : in String;
+                             Model     : in String;
+                             Config    : in ADO.Sessions.Sources.Data_Source) return String;
 
-   --  Check if the database with the given name exists.
-   function Has_Database (DB   : in ADO.Sessions.Session'Class;
-                          Name : in String) return Boolean;
-
-   --  Check if the database with the given name has some tables.
-   function Has_Tables (DB   : in ADO.Sessions.Session'Class;
-                        Name : in String) return Boolean;
-
-   --  Execute the external command <b>Name</b> with the arguments in <b>Args</b>
-   --  and send the content of the file <b>Input</b> to that command.
-   procedure Execute_Command (Command : in String;
-                              Input   : in String);
-
-   --  Create the MySQL tables in the database.  The tables are created by launching
-   --  the external command 'mysql' and using the create-xxx-mysql.sql generated scripts.
-   procedure Create_Mysql_Tables (Name   : in String;
-                                  Model  : in String;
-                                  Config : in ADO.Sessions.Sources.Data_Source;
-                                  Generator : in out Gen.Generator.Handler);
-
-   --  Create the database identified by the given name.
-   procedure Create_Database (DB   : in ADO.Sessions.Master_Session;
-                              Name : in String);
-
-   --  Create the user and grant him access to the database.
-   procedure Create_User_Grant (DB       : in ADO.Sessions.Master_Session;
-                                Name     : in String;
-                                User     : in String;
-                                Password : in String);
-
-   --  ------------------------------
-   --  Check if the database with the given name exists.
-   --  ------------------------------
-   function Has_Database (DB   : in ADO.Sessions.Session'Class;
-                          Name : in String) return Boolean is
-      Query : ADO.Queries.Context;
-      Stmt  : ADO.Statements.Query_Statement;
+   function Get_Schema_Path (Model_Dir : in String;
+                             Model     : in String;
+                             Config    : in ADO.Sessions.Sources.Data_Source) return String is
+      Driver : constant String := Config.Get_Driver;
+      Dir    : constant String := Util.Files.Compose (Model_Dir, Driver);
    begin
-      Query.Set_Query (Gen.Database.Model.Query_Database_List);
-      Stmt := DB.Create_Statement (Query);
-      Stmt.Execute;
-      while Stmt.Has_Elements loop
-         declare
-            D : constant String := Stmt.Get_String (0);
-         begin
-            if Name = D then
-               return True;
-            end if;
-         end;
-         Stmt.Next;
-      end loop;
-      return False;
-   end Has_Database;
-
-   --  ------------------------------
-   --  Check if the database with the given name has some tables.
-   --  ------------------------------
-   function Has_Tables (DB   : in ADO.Sessions.Session'Class;
-                        Name : in String) return Boolean is
-      Query : ADO.Queries.Context;
-      Stmt  : ADO.Statements.Query_Statement;
-   begin
-      Query.Set_Query (Gen.Database.Model.Query_Table_List);
-      Stmt := DB.Create_Statement (Query);
-      Stmt.Bind_Param ("name", ADO.Parameters.Token (Name));
-      Stmt.Execute;
-      return Stmt.Has_Elements;
-   end Has_Tables;
-
-   --  ------------------------------
-   --  Create the database identified by the given name.
-   --  ------------------------------
-   procedure Create_Database (DB   : in ADO.Sessions.Master_Session;
-                              Name : in String) is
-      Query : ADO.Queries.Context;
-      Stmt  : ADO.Statements.Query_Statement;
-   begin
-      Log.Info ("Creating database '{0}'", Name);
-
-      Query.Set_Query (Gen.Database.Model.Query_Create_Database);
-      Stmt := DB.Create_Statement (Query);
-      Stmt.Bind_Param ("name", ADO.Parameters.Token (Name));
-      Stmt.Execute;
-
-   end Create_Database;
-
-   --  ------------------------------
-   --  Create the user and grant him access to the database.
-   --  ------------------------------
-   procedure Create_User_Grant (DB       : in ADO.Sessions.Master_Session;
-                                Name     : in String;
-                                User     : in String;
-                                Password : in String) is
-      Query : ADO.Queries.Context;
-      Stmt  : ADO.Statements.Query_Statement;
-   begin
-      Log.Info ("Granting access for user '{0}' to database '{1}'", User, Name);
-
-      if Password'Length > 0 then
-         Query.Set_Query (Gen.Database.Model.Query_Create_User_With_Password);
-      else
-         Query.Set_Query (Gen.Database.Model.Query_Create_User_No_Password);
-      end if;
-
-      Stmt := DB.Create_Statement (Query);
-      Stmt.Bind_Param ("name", ADO.Parameters.Token (Name));
-      Stmt.Bind_Param ("user", ADO.Parameters.Token (User));
-      if Password'Length > 0 then
-         Stmt.Bind_Param ("password", Password);
-      end if;
-      Stmt.Execute;
-
-      Query.Set_Query (Gen.Database.Model.Query_Flush_Privileges);
-      Stmt := DB.Create_Statement (Query);
-      Stmt.Execute;
-   end Create_User_Grant;
-
-   --  ------------------------------
-   --  Execute the external command <b>Name</b> with the arguments in <b>Args</b>
-   --  and send the content of the file <b>Input</b> to that command.
-   --  ------------------------------
-   procedure Execute_Command (Command : in String;
-                              Input   : in String) is
-      Proc    : aliased Util.Streams.Pipes.Pipe_Stream;
-      Text    : Util.Streams.Texts.Reader_Stream;
-   begin
-      if Input /= "" then
-         Proc.Set_Input_Stream (Input);
-      end if;
-      Text.Initialize (Proc'Unchecked_Access);
-      Proc.Open (Command, Util.Processes.READ);
-      while not Text.Is_Eof loop
-         declare
-            Line : Ada.Strings.Unbounded.Unbounded_String;
-         begin
-            Text.Read_Line (Line, Strip => False);
-            exit when Ada.Strings.Unbounded.Length (Line) = 0;
-            Log.Error ("{0}", Ada.Strings.Unbounded.To_String (Line));
-         end;
-      end loop;
-      Proc.Close;
-      if Proc.Get_Exit_Status = 0 then
-         Log.Info ("Database schema created successfully.");
-      elsif Proc.Get_Exit_Status = 255 then
-         Log.Error ("Command not found: {0}", Command);
-      else
-         Log.Error ("Command {0} failed with exit code {1}", Command,
-                    Util.Strings.Image (Proc.Get_Exit_Status));
-      end if;
-
-   exception
-      when Ada.IO_Exceptions.Name_Error =>
-         Log.Error ("Cannot read {0}", Input);
-   end Execute_Command;
-
-   --  ------------------------------
-   --  Create the MySQL tables in the database.  The tables are created by launching
-   --  the external command 'mysql' and using the create-xxx-mysql.sql generated scripts.
-   --  ------------------------------
-   procedure Create_Mysql_Tables (Name      : in String;
-                                  Model     : in String;
-                                  Config    : in ADO.Sessions.Sources.Data_Source;
-                                  Generator : in out Gen.Generator.Handler) is
-      Database : constant String := Config.Get_Database;
-      Username : constant String := Config.Get_Property ("user");
-      Password : constant String := Config.Get_Property ("password");
-      Dir      : constant String := Util.Files.Compose (Model, "mysql");
-      File     : constant String := Util.Files.Compose (Dir, "create-" & Name & "-mysql.sql");
-   begin
-      Log.Info ("Creating database tables using schema '{0}'", File);
-
-      if not Ada.Directories.Exists (File) then
-         Generator.Error ("SQL file '{0}' does not exist.", File);
-         Generator.Error ("Please, run the following command: dynamo generate db");
-         return;
-      end if;
-
-      if Password'Length > 0 then
-         Execute_Command ("mysql --user=" & Username & " --password=" & Password & " "
-                          & Database, File);
-      else
-         Execute_Command ("mysql --user=" & Username & " "
-                          & Database, File);
-      end if;
-   end Create_Mysql_Tables;
+      return Util.Files.Compose (Dir, "create-" & Model & "-" & Driver & ".sql");
+   end Get_Schema_Path;
 
    --  ------------------------------
    --  Execute the command with the arguments.
@@ -239,149 +58,61 @@ package body Gen.Commands.Database is
                       Generator : in out Gen.Generator.Handler) is
       pragma Unreferenced (Cmd, Name);
 
-      use Ada.Strings.Unbounded;
-
-      procedure Create_Database (Model    : in String;
-                                 Database : in String;
-                                 Username : in String;
-                                 Password : in String);
-      procedure Create_MySQL_Database (Model    : in String;
-                                       Config   : in ADO.Sessions.Sources.Data_Source;
-                                       Database : in String;
-                                       Username : in String;
-                                       Password : in String);
-      procedure Create_SQLite_Database (Model    : in String;
-                                        Config   : in ADO.Sessions.Sources.Data_Source;
-                                        Database : in String);
+      procedure Create_Database (Model_Dir : in String;
+                                 Database  : in String;
+                                 Username  : in String;
+                                 Password  : in String);
 
       --  ------------------------------
       --  Create the database, the user and the tables.
       --  ------------------------------
-      procedure Create_MySQL_Database (Model    : in String;
-                                       Config   : in ADO.Sessions.Sources.Data_Source;
-                                       Database : in String;
-                                       Username : in String;
-                                       Password : in String) is
-         Root_Config     : ADO.Sessions.Sources.Data_Source := Config;
-         Factory         : ADO.Sessions.Factory.Session_Factory;
-         Root_Connection : Unbounded_String;
-         Root_Hidden     : Unbounded_String;
-         Pos             : Natural;
-      begin
-         --  Build a connection string to create the database.
-         Pos := Util.Strings.Index (Database, ':');
-         Append (Root_Connection, Database (Database'First .. Pos));
-         Append (Root_Connection, "//");
-         Append (Root_Connection, Config.Get_Server);
-         if Config.Get_Port > 0 then
-            Append (Root_Connection, ':');
-            Append (Root_Connection, Util.Strings.Image (Config.Get_Port));
-         end if;
-         Append (Root_Connection, "/?user=");
-         Append (Root_Connection, Username);
-         Root_Hidden := Root_Connection;
-         if Password'Length > 0 then
-            Append (Root_Connection, "&password=");
-            Root_Hidden := Root_Connection;
-            Append (Root_Connection, Password);
-            Append (Root_Hidden, "XXXXXXXX");
-         else
-            Root_Config.Set_Property ("password", "");
-         end if;
-
-         Log.Info ("Connecting to {0} for database setup", Root_Hidden);
-
-         --  Initialize the session factory to connect to the
-         --  database defined by root connection (which should allow the database creation).
-         Root_Config.Set_Connection (To_String (Root_Connection));
-         Factory.Create (Root_Config);
-
-         declare
-            Name : constant String := Generator.Get_Project_Name;
-            DB   : constant ADO.Sessions.Master_Session := Factory.Get_Master_Session;
-         begin
-            --  Create the database only if it does not already exists.
-            if not Has_Database (DB, Config.Get_Database) then
-               Create_Database (DB, Config.Get_Database);
-            end if;
-
-            --  If some tables exist, don't try to create tables again.
-            --  We could improve by reading the current database schema, comparing with our
-            --  schema and create what is missing (new tables, new columns).
-            if Has_Tables (DB, Config.Get_Database) then
-               Generator.Error ("The database {0} exists", Config.Get_Database);
-            else
-               if Username /= Config.Get_Property ("user") then
-                  --  Create the user grant.  On MySQL, it is safe to do this several times.
-                  Create_User_Grant (DB, Config.Get_Database,
-                                     Config.Get_Property ("user"),
-                                     Config.Get_Property ("password"));
-               end if;
-
-               --  And now create the tables by using the SQL script generated by Dyanmo.
-               Create_Mysql_Tables (Name, Model, Config, Generator);
-            end if;
-         end;
-      end Create_MySQL_Database;
-
-      --  ------------------------------
-      --  Create the SQLite database.
-      --  ------------------------------
-      procedure Create_SQLite_Database (Model    : in String;
-                                        Config   : in ADO.Sessions.Sources.Data_Source;
-                                        Database : in String) is
-         Name  : constant String := Generator.Get_Project_Name;
-         Path  : constant String := Config.Get_Database;
-         Dir   : constant String := Util.Files.Compose (Model, "sqlite");
-         File  : constant String := Util.Files.Compose (Dir, "create-" & Name & "-sqlite.sql");
-         Cfg   : constant String := Generator.Get_Config_Directory;
-         Cmd   : constant String := Util.Files.Compose (Cfg, "sqlite-exit.sql");
-      begin
-         if Ada.Directories.Exists (Path) then
-            Log.Info ("Connecting to {0} for database setup", Database);
-         end if;
-
-         --  Initialize the session factory to connect to the
-         --  database defined by root connection (which should allow the database creation).
-         Log.Info ("Creating database tables using schema '{0}'", File);
-
-         if not Ada.Directories.Exists (File) then
-            Generator.Error ("SQL file '{0}' does not exist.", File);
-            Generator.Error ("Please, run the following command: dynamo generate db");
-            return;
-         end if;
-
-         Execute_Command ("sqlite3 --batch --init " & File & " " & Path, Cmd);
-      end Create_SQLite_Database;
-
-      --  ------------------------------
-      --  Create the database, the user and the tables.
-      --  ------------------------------
-      procedure Create_Database (Model    : in String;
-                                 Database : in String;
-                                 Username : in String;
-                                 Password : in String) is
-         Config          : ADO.Sessions.Sources.Data_Source;
+      procedure Create_Database (Model_Dir : in String;
+                                 Database  : in String;
+                                 Username  : in String;
+                                 Password  : in String) is
+         Admin    : ADO.Sessions.Sources.Data_Source;
+         Config   : ADO.Sessions.Sources.Data_Source;
+         Messages : Util.Strings.Vectors.Vector;
       begin
          Config.Set_Connection (Database);
-         Config.Set_Property ("ado.queries.paths", Generator.Get_Parameter ("ado.queries.paths"));
+         Admin := Config;
          if Config.Get_Database = "" then
             Generator.Error ("Invalid database connection: no database name specified");
             return;
          end if;
 
-         if Config.Get_Driver = "mysql" then
-            if Config.Get_Property ("user") = "" then
-               Generator.Error ("Invalid database connection: missing user property");
+         declare
+            Name : constant String := Generator.Get_Project_Name;
+            Path : constant String := Get_Schema_Path (Model_Dir, Name, Config);
+         begin
+            Log.Info ("Creating database tables using schema '{0}'", Path);
+
+            if not Ada.Directories.Exists (Path) then
+               Generator.Error ("SQL file '{0}' does not exist.", Path);
+               Generator.Error ("Please, run the following command: dynamo generate db");
                return;
             end if;
-            Create_MySQL_Database (Model, Config, Database, Username, Password);
-         elsif Config.Get_Driver = "sqlite" then
-            Create_SQLite_Database (Model, Config, Database);
-         else
-            Generator.Error ("Database driver {0} is not supported.", Config.Get_Driver);
-         end if;
 
+            if Config.Get_Driver = "mysql" or Config.Get_Driver = "postgresql" then
+               if Config.Get_Property ("user") = "" then
+                  Generator.Error ("Invalid database connection: missing user property");
+                  return;
+               end if;
+               Admin.Set_Property ("user", Username);
+               Admin.Set_Property ("password", Password);
+
+            elsif Config.Get_Driver /= "sqlite" then
+               Generator.Error ("Database driver {0} is not supported.", Config.Get_Driver);
+               return;
+            end if;
+            Admin.Set_Database ("");
+            ADO.Schemas.Databases.Create_Database (Admin, Config, Path, Messages);
+
+            --  Report the messages
+            for Msg of Messages loop
+               Log.Error ("{0}", Msg);
+            end loop;
+         end;
          --  Remember the database connection string.
          Generator.Set_Project_Property ("database", Database);
          Generator.Save_Project;

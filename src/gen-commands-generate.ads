f2c66ba99bd0dc5aaf84b5764b59b24e6f5486f4
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-27 17:40:42 +0100

    Add Name parameter to the Help procedure

--- src/gen-commands-generate.ads
+++ src/gen-commands-generate.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  gen-commands-generate -- Generate command for dynamo
---  Copyright (C) 2011, 2017, 2018 Stephane Carrez
+--  Copyright (C) 2011, 2017, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,6 +33,7 @@ package Gen.Commands.Generate is
    --  Write the help associated with the command.
    overriding
    procedure Help (Cmd       : in out Command;
+                   Name      : in String;
                    Generator : in out Gen.Generator.Handler);
 
 end Gen.Commands.Generate;

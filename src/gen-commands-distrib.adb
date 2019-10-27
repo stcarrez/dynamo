928c36bcb13de8c5ecc1e9249b679822bcaa4ec0
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-27 17:40:38 +0100

    Add Name parameter to the Help procedure

--- src/gen-commands-distrib.adb
+++ src/gen-commands-distrib.adb
@@ -56,8 +56,9 @@ package body Gen.Commands.Distrib is
    --  ------------------------------
    overriding
    procedure Help (Cmd       : in out Command;
+                   Name      : in String;
                    Generator : in out Gen.Generator.Handler) is
-      pragma Unreferenced (Cmd, Generator);
+      pragma Unreferenced (Cmd, Name, Generator);
       use Ada.Text_IO;
    begin
       Put_Line ("dist: Build the distribution files to prepare the server installation");

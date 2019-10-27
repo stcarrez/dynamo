e5f230684e0ba89d597975cf331fabd064eb6019
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-27 17:40:23 +0100

    Add Name parameter to the Help procedure

--- src/gen-commands-page.adb
+++ src/gen-commands-page.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  gen-commands-page -- Page creation command for dynamo
---  Copyright (C) 2011, 2012, 2013, 2014, 2017, 2018 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2014, 2017, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -86,8 +86,9 @@ package body Gen.Commands.Page is
    --  ------------------------------
    overriding
    procedure Help (Cmd       : in out Command;
+                   Name      : in String;
                    Generator : in out Gen.Generator.Handler) is
-      pragma Unreferenced (Cmd);
+      pragma Unreferenced (Cmd, Name);
       use Ada.Text_IO;
       use Ada.Directories;
 

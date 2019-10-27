d7c1914cd3ec33837812bf80bd571faabc222d67
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-27 17:40:18 +0100

    Add Name parameter to the Help procedure

--- src/gen-commands-project.adb
+++ src/gen-commands-project.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  gen-commands-project -- Project creation command for dynamo
---  Copyright (C) 2011, 2012, 2013, 2014, 2017, 2018 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2014, 2017, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -211,8 +211,9 @@ package body Gen.Commands.Project is
    --  ------------------------------
    overriding
    procedure Help (Cmd       : in out Command;
+                   Name      : in String;
                    Generator : in out Gen.Generator.Handler) is
-      pragma Unreferenced (Cmd, Generator);
+      pragma Unreferenced (Cmd, Name, Generator);
       use Ada.Text_IO;
    begin
       Put_Line ("create-project: Create a new Ada Web Application project");

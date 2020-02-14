97439a65c2e069eec91e2a8124a9ec2cc3a82192
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-14 23:40:17 +0100

    Add --quiet for coverage

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 NAME=dynamo.cov
-lcov --base-directory . --directory . -c --include "*/dynamo/src/*" -o $NAME
-lcov --remove $NAME "*/src/gpr/*" -o $NAME
-lcov --remove $NAME "*/src/gnat/*" -o $NAME
-lcov --remove $NAME "*/src/yaml/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c --include "*/dynamo/src/*" -o $NAME
+lcov --quiet --remove $NAME "*/src/gpr/*" -o $NAME
+lcov --quiet --remove $NAME "*/src/gnat/*" -o $NAME
+lcov --quiet --remove $NAME "*/src/yaml/*" -o $NAME
 rm -rf cover
-genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
+genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  

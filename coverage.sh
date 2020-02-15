432746741478347202f70d5a203870f74cd9d24a
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-15 14:31:47 +0100

    Revert to avoid using --include and --exclude option since old version of lcov do not support these options

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 NAME=dynamo.cov
-lcov --quiet --base-directory . --directory . -c --include "*/dynamo/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c -o $NAME
+lcov --quiet --remove $NAME "/usr*" -o $NAME
+lcov --quiet --remove $NAME "/build*" -o $NAME
+lcov --quiet --remove $NAME "/opt*" -o $NAME
+lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
+lcov --quiet --remove $NAME "*/regtests*" -o $NAME
+lcov --quiet --remove $NAME "*/b__*" -o $NAME
 lcov --quiet --remove $NAME "*/src/gpr/*" -o $NAME
 lcov --quiet --remove $NAME "*/src/gnat/*" -o $NAME
 lcov --quiet --remove $NAME "*/src/yaml/*" -o $NAME

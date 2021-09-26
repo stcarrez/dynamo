95b97ee5993616cedff48465d224e07142b57c75
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-09-26 16:45:28 +0200

    Fix #9: add a Project parameter to Prepare

--- src/gen-artifacts.ads
+++ src/gen-artifacts.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  gen-artifacts -- Artifacts for Code Generator
---  Copyright (C) 2011, 2012, 2018 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2018, 2021 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -85,6 +85,7 @@ package Gen.Artifacts is
    --  actually invoking the generation.
    procedure Prepare (Handler : in out Artifact;
                       Model   : in out Gen.Model.Packages.Model_Definition'Class;
+                      Project : in out Gen.Model.Projects.Project_Definition'Class;
                       Context : in out Generator'Class) is null;
 
    --  After the generation, perform a finalization step for the generation process.

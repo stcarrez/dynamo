51609ba9fcb7467fa3c4ae6f247d75c86158a0fe
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-11-13 11:48:15 +0100

    Declare the UString subtype to simplify the implementation and use of Unbounded_String

--- src/gen.ads
+++ src/gen.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  Gen -- Code Generator
---  Copyright (C) 2009, 2010, 2011 Stephane Carrez
+--  gen -- Code Generator
+--  Copyright (C) 2009, 2010, 2011, 2021 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Ada.Strings.Unbounded;
 package Gen is
 
    --  Library SVN identification
@@ -23,5 +23,21 @@ package Gen is
 
    --  Revision used (must run 'make version' to update)
    SVN_REV : constant Positive := 1095;
+
+   subtype UString is Ada.Strings.Unbounded.Unbounded_String;
+
+   function To_UString (Value : in String) return UString
+     renames Ada.Strings.Unbounded.To_Unbounded_String;
+
+   function To_String (Value : in UString) return String
+     renames Ada.Strings.Unbounded.To_String;
+
+   function Length (Value : in UString) return Natural
+     renames Ada.Strings.Unbounded.Length;
+
+   function "=" (Left, Right : in UString) return Boolean
+     renames Ada.Strings.Unbounded."=";
+   function "=" (Left : in UString; Right : in String) return Boolean
+     renames Ada.Strings.Unbounded."=";
 
 end Gen;

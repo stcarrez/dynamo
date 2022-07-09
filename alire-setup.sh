4df15b4fe0f3bc56763beb975784eaaced3e1529
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-07-09 17:39:48 +0200

    Try fixing the alire setup script

--- alire-setup.sh
+++ alire-setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 INSTALL_DIR=`pwd`
 gnatprep -DCONFIG_DIR=\"${INSTALL_DIR}/config\" -DVERSION=\"1.2.3\" src/gen-configs.gpb src/gen-configs.ads
+sed -e 's,@GNAT_PROJECT_PATHS@,.,' \
+    -e 's,@DYNAMO_SEARCH_DIR@,.,' \
+    -e "s,@BUNDLE_DIR@,${INSTALL_DIR}/bundles," \
+    config/generator.properties.in > config/generator.properties

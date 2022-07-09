5393d6504fa0fc8ab2f945aed91630b2fea33655
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-07-09 17:26:03 +0200

    Fix the alire setup script

--- alire-setup.sh
+++ alire-setup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-INSTALL_DIR=`pwd`/config
-echo "ROOT=${CRATE_ROOT}"
+INSTALL_DIR=`pwd`
 gnatprep -DCONFIG_DIR=\"${INSTALL_DIR}/config\" -DVERSION=\"1.2.3\" src/gen-configs.gpb src/gen-configs.ads

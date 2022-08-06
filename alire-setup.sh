f20748f2fc87057483502f36f3c5089d0fdb332f
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-08-06 15:54:23 +0200

    Fix script to use version 1.3.0

--- alire-setup.sh
+++ alire-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 INSTALL_DIR=`pwd`
-gnatprep -DCONFIG_DIR=\"${INSTALL_DIR}/config\" -DVERSION=\"1.2.3\" src/gen-configs.gpb src/gen-configs.ads
+gnatprep -DCONFIG_DIR=\"${INSTALL_DIR}/config\" -DVERSION=\"1.3.0\" src/gen-configs.gpb src/gen-configs.ads
 sed -e 's,@GNAT_PROJECT_PATHS@,.,' \
     -e 's,@DYNAMO_SEARCH_DIR@,.,' \
     -e "s,@BUNDLE_DIR@,.," \

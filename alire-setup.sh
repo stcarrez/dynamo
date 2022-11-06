2812b1e7ba77b683e46e2db969517dc12c0295d3
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-11-06 15:15:11 +0100

    Fix the alire setup script so that we have a default configuration to search for GNAT projects

--- alire-setup.sh
+++ alire-setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 INSTALL_DIR=`pwd`
 gnatprep -DCONFIG_DIR=\"${INSTALL_DIR}/config\" -DVERSION=\"1.3.0\" src/gen-configs.gpb src/gen-configs.ads
-sed -e 's,@GNAT_PROJECT_PATHS@,.,' \
+GNATBUILD=`which gprbuild`
+GNATBIN=`dirname ${GNATBUILD}`
+GNATDIR=`dirname ${GNATBIN}`
+GNAT_PROJECT_PATHS="${GNATDIR}/share/gpr"
+sed -e "s,@GNAT_PROJECT_PATHS@,${GNAT_PROJECT_PATHS}," \
     -e 's,@DYNAMO_SEARCH_DIR@,.,' \
     -e "s,@BUNDLE_DIR@,.," \
     config/generator.properties.in > config/generator.properties

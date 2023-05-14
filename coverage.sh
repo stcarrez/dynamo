0dbb6554422401deaeae5294160f342fc8d0121b
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-05-14 14:44:41 +0200

    Cleanup and simplify the coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 NAME=dynamo.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/b__*" -o $NAME
-lcov --quiet --remove $NAME "*/src/gpr/*" -o $NAME
-lcov --quiet --remove $NAME "*/src/gnat/*" -o $NAME
-lcov --quiet --remove $NAME "*/src/yaml/*" -o $NAME
+lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/regtests*' \
+   --exclude '*/gnat/*' \
+   --exclude '*/yaml/*' \
+   --exclude '*/src/gpr/*' -c -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
- 
+lcov --summary dynamo.cov

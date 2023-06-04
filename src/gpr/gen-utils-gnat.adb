f5df9e676ebaadcd785979dcf03b060ac227961c
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-06-04 15:01:50 +0200

    Update to use the GPR.Conf.Parse_Project_And_Apply_Config API

--- src/gpr/gen-utils-gnat.adb
+++ src/gpr/gen-utils-gnat.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  gen-utils-gnat -- GNAT utilities
---  Copyright (C) 2011, 2012, 2014, 2015, 2018, 2022 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2014, 2015, 2018, 2022, 2023 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,15 +22,15 @@ with Gen.Configs;
 
 with GPR.Snames;
 with GPR.Names;
-with GPR.Part;
 with GPR.Tree;
 with GPR.Env;
 with GPR.Util;
-with GPR.Proc;
+with GPR.Conf;
+with GPR.Err;
 with Gpr_Build_Util;
 with GPR.Output;
 with GPR.Sdefault;
-with GNAT.OS_Lib; use GNAT.OS_Lib;
+with GNAT.OS_Lib; use GNAT;
 
 package body Gen.Utils.GNAT is
 
@@ -140,36 +140,38 @@ package body Gen.Utils.GNAT is
       procedure For_All_Projects is
         new GPR.For_Every_Project_Imported (Boolean, Recursive_Add);
 
+      use type GPR.Project_Id;
+
+      Dummy  : Boolean := False;
+      Config : OS_Lib.String_Access;
    begin
       Log.Info ("Reading GNAT project {0}", Project_File_Name);
 
       --  Parse the GNAT project files and build the tree.
-      GPR.Part.Parse (Project           => Project_Node,
-                      In_Tree           => Project_Node_Tree,
-                      Project_File_Name => Project_File_Name,
-                      Packages_To_Check => null,
-                      Is_Config_File    => False,
-                      Env               => Gpr_Build_Util.Root_Environment);
+      Main_Project := GPR.No_Project;
+      GPR.Conf.Parse_Project_And_Apply_Config
+        (Main_Project => Main_Project,
+         User_Project_Node => Project_Node,
+         Config_File_Name => "",
+         Autoconf_Specified => False,
+         Project_File_Name => Project_File_Name,
+         Project_Tree => Project_Tree,
+         Project_Node_Tree => Project_Node_Tree,
+         Env => Gpr_Build_Util.Root_Environment,
+         Automatically_Generated => Dummy,
+         Config_File_Path => Config,
+         Packages_To_Check => null,
+         Target_Name => "",
+         Normalized_Hostname => "");
+      if Main_Project = GPR.No_Project then
+         GPR.Err.Finalize;
+         return;
+      end if;
 
       if not GPR.Tree.No (Project_Node) then
-         declare
-            Dummy : Boolean := False;
-            Success : Boolean;
-         begin
-            GPR.Proc.Process_Project_Tree_Phase_1 (In_Tree => Project_Tree,
-                                                   Project => Main_Project,
-                                                   Packages_To_Check => null,
-                                                   From_Project_Node => Project_Node,
-                                                   From_Project_Node_Tree => Project_Node_Tree,
-                                                   Env => Gpr_Build_Util.Root_Environment,
-                                                   Reset_Tree => True,
-                                                   Success => Success,
-                                                   On_New_Tree_Loaded => null);
-
-            --  Scan the tree to get the list of projects (in dependency order).
-            For_All_Projects (Main_Project, Project_Tree,
-                              Imported_First => True, With_State => Dummy);
-         end;
+         --  Scan the tree to get the list of projects (in dependency order).
+         For_All_Projects (Main_Project, Project_Tree, Include_Aggregated => True,
+                           Imported_First => True, With_State => Dummy);
       end if;
    end Read_GNAT_Project_List;
 

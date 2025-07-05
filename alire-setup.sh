b61c5daa035175185dd92a33e40255a7f28029af
Stephane Carrez <Stephane.Carrez@gmail.com>
2025-07-05 17:37:06 +0200

    Bug #31: update installation of dynamo
    - fix configuration for generator.dynamo.dir

--- alire-setup.sh
+++ alire-setup.sh
@@ -6,6 +6,6 @@ GNATBIN=`dirname ${GNATBUILD}`
 GNATDIR=`dirname ${GNATBIN}`
 GNAT_PROJECT_PATHS="${GNATDIR}/share/gpr"
 test -f config/generator.properties || sed -e "s,@GNAT_PROJECT_PATHS@,${GNAT_PROJECT_PATHS}," \
-    -e 's,@DYNAMO_SEARCH_DIR@,.,' \
+    -e 's,@DYNAMO_SEARCH_DIR@,.;#{generator_config_dir},' \
     -e "s,@BUNDLE_DIR@,.;#{generator_config_dir}/../asf/bundles," \
     config/generator.properties.in > config/generator.properties
